668b1e4129832941a8bb1d55d0ee249391d5f1dc
AMordelle <wilcas0207@gmail.com>
2026-04-21 21:52:07 -0600

    Implement daily MLB analysis orchestration use-case

--- src/features/mlb/application/use-cases/getTopOpportunities.ts
+++ src/features/mlb/application/use-cases/getTopOpportunities.ts
@@ -1,3 +1,8 @@
-export async function getTopOpportunities() {
-  throw new Error("Not implemented");
+import type { GameAnalysisResult } from "../dto/types";
+import { rankOpportunities } from "../../domain/services/rankOpportunities";
+
+export function getTopOpportunities(results: GameAnalysisResult[]): GameAnalysisResult[] {
+  const bettable = results.filter((result) => result.recommendation !== "NO_BET");
+
+  return rankOpportunities(bettable);
 }
